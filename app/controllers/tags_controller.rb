800fa94079581090c67b88015796a73f001058a7
shullmb <dev.shullmb@gmail.com>
2015-08-28 17:23:05 -0700

    Tagging feature complete

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+  
+  def index
+    @tags = Tag.all
+  end
+  
+  def destroy
+    @tag = Tag.destroy(params[:id])
+    redirect_to tags_path
+    
+    flash.notice = "Tag '#{@tag.name}' deleted"    
+  end
+end
